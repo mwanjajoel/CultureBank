8f16bbab67b3fa99b545ccf663302d4f288b400d
mwanjajoel <joelsilverworks@gmail.com>
2017-01-15 21:41:32 +0300

    Initial running code where posts can be created.

---
+++ cultureenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/macbookair/Downloads/PythonProjects/culturebank/cultureenv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
